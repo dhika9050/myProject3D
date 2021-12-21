9ffb1595139ffa691e996a7e7f7a1e5975584847
dhika9050 <96412333+dhika9050@users.noreply.github.com>
2021-12-21 11:34:28 +0700

    Adding Declaration

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	private CharacterControl chara_control;
+	private Vector3 move_Direct;
+
+	public fload speed = 5f;
+	private float gravity = 20f;
+
+	public float jump_Force = 10f;
+	private float vertical_velocity;
+}
