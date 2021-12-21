b7849ab7c628b859eb206b2c3c3152bf46f15452
dhika9050 <96412333+dhika9050@users.noreply.github.com>
2021-12-21 11:58:03 +0700

    Repairing Code

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -7,7 +7,7 @@ public class PlayerMovement : MonoBehaviour
 	private CharacterController character_Controller;
 	private Vector3 move_Direction;
 
-	public fload speed = 5f;
+	public float speed = 5f;
 	private float gravity = 20f;
 
 	public float jump_Force = 10f;
