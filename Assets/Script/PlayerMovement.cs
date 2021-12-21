8c6374a228448b142009e6fbab94482e59075f13
dhika9050 <96412333+dhika9050@users.noreply.github.com>
2021-12-21 11:56:37 +0700

    Adding Movement

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -4,12 +4,55 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-	private CharacterControl chara_control;
-	private Vector3 move_Direct;
+	private CharacterController character_Controller;
+	private Vector3 move_Direction;
 
 	public fload speed = 5f;
 	private float gravity = 20f;
 
 	public float jump_Force = 10f;
 	private float vertical_velocity;
+
+	void Awake() {
+        character_Controller = GetComponent<CharacterController>();
+    }
+	
+	void Update () {
+        MoveThePlayer();
+	}
+
+    void MoveThePlayer() {
+
+        move_Direction = new Vector3(Input.GetAxis(Axis.HORIZONTAL), 0f,
+                                     Input.GetAxis(Axis.VERTICAL));
+
+        move_Direction = transform.TransformDirection(move_Direction);
+        move_Direction *= speed * Time.deltaTime;
+
+        ApplyGravity();
+
+        character_Controller.Move(move_Direction);
+
+
+    } // move player
+
+    void ApplyGravity() {
+
+        vertical_Velocity -= gravity * Time.deltaTime;
+
+        // jump
+        PlayerJump();
+
+        move_Direction.y = vertical_Velocity * Time.deltaTime;
+
+    } // apply gravity
+
+    void PlayerJump() {
+
+        if(character_Controller.isGrounded && Input.GetKeyDown(KeyCode.Space)) {
+            vertical_Velocity = jump_Force;
+        }
+
+    }
+
 }
